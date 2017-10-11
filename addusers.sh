c302c6d8b239da439ff40fd8d56b89b9416d2f0e
davecleo <dbrunswick@cleo.com>
2017-10-10 18:32:25 -0700

    Update addusers.sh
    Tidied up variable names for ease of editing

--- addusers.sh
+++ addusers.sh
@@ -4,13 +4,15 @@ declare -i last_user
 user_base_name="User"
 bank_base_name="Bank"
 user_password="cleo"
+number_banks=300
+users_per_bank=20
 newline=$'\n'
 
-for bank in {1..300}
+for bank_num in $(seq 1 ${number_banks})
 do
-    first_user=($bank-1)*20+1
-    last_user=$bank*20
-    commands+="${newline}user ${user_base_name}[${first_user}..${last_user}]:${user_password}@${bank_base_name}${bank} Folderpermission=root,overwrite,upload,download,delete user:local/root/%username"
+    first_user=(${bank_num}-1)*${users_per_bank}+1
+    last_user=${bank_num}*${users_per_bank}
+    commands+="${newline}user ${user_base_name}[${first_user}..${last_user}]:${user_password}@${bank_base_name}${bank_num} Folderpermission=root,overwrite,upload,download,delete user:local/root/%username"
 done
 /usr/local/bin/shell.sh << SHELL_FINISH
 $commands
